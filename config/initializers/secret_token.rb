e74ed04b6022df188ca56311ec6b3908f0a56b5b
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@mura.com.br>
2012-03-15 12:23:59 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flog::Application.config.secret_token = '1c5161860c5f0a54487fe77173b71aefdb1cf95384cbcacdb6a47dae488c071192a4727c6efb3b7da3e3180e90d5f641a0269b719631d4a0b0d1cdf01031af77'
