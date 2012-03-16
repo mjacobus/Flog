5611cd2851bf6fedd2c0a81eaccc5227f293c0a4
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-03-16 13:18:26 -0300

    Added PostsController and routing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Flog::Application.routes.draw do
+
+  root :to => "posts#index", :page => '1'
+  
+  # posts by page
+  match '/page(/:page)' => "posts#index",
+    :page => /\d{1,}/, 
+    :defaults => {:page => '1'}
+    
+  # posts by date
+  match '/:year(/:month(/:day))(/page/:page)' => "posts#index",
+    :year => /\d{4}/, 
+    :month => /\d{1,2}/,
+    :day => /\d{1,2}/,
+    :page => /\d{1,}/, 
+    :defaults => {:page => '1'}  
+    
+   # posts by tag
+  match '/tag/:tag(/page/:page)' => "posts#index",
+    :tag => /[\w-]+/, 
+    :page => /\d{1,}/, 
+    :defaults => {:page => '1'}  
+
+
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
---
+++ spec/routing/posts_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+
+describe PostsController do
+  
+  # the root of the site
+  context 'GET /' do
+    it "should redirect respond to #index" do
+      {:get => '/'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :page => '1'
+      )
+    end
+  end
+  
+  # listing posts by page
+  context 'listing by page' do
+    it 'GET /page/7 should route to #index?page=7' do
+      {:get => '/page/7'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :page => '7'
+      )
+    end
+    
+    it 'GET /page should route to #index?page=1' do
+      {:get => '/page'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :page => '1'
+      )
+    end
+  end
+  
+  
+  # listing posts by date
+  context 'listing by date' do
+    it 'GET /2012/11/06 should route to #index?year=2012&month=11&day=06' do
+      {:get => '/2012/11/06'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :year => '2012',
+        :month => '11',
+        :day => '06',
+        :page => '1'
+      )        
+    end
+    
+    it 'GET /2012/11/06/page/7 should route to #index?year=2012&month=11&day=06&page=7' do
+      {:get => '/2012/11/06/page/7'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :year => '2012',
+        :month => '11',
+        :day => '06',
+        :page => '7'
+      )        
+    end
+    
+    it 'GET /2012/11 should route to #index?year=2012&month=11' do
+      {:get => '/2012/11'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :year => '2012',
+        :month => '11',
+        :page => '1'
+      )        
+    end
+    
+    it 'GET /2012/11/page/7 should route to #index?year=2012&month=11&page=7' do
+      {:get => '/2012/11/page/7'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :year => '2012',
+        :month => '11',
+        :page => '7'
+      )        
+    end
+    
+    it 'GET /2012 should route to #index?year=2012&month=11&day=06' do
+      {:get => '/2012'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :year => '2012',
+        :page => '1'
+      )        
+    end
+    
+    it 'GET /2012/page/7 should route to #index?year=2012&month=11&day=06&page=7' do
+      {:get => '/2012/page/7'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :year => '2012',
+        :page => '7'
+      )        
+    end
+  end
+  
+  context "listint by tag" do
+    it "GET /tag/the-tag-name should route to #index?tag=the-tag-name&page=1" do
+      {:get => '/tag/the-tag-name'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :tag => 'the-tag-name',
+        :page => '1'
+      )
+    end
+    
+   it "GET /tag/the-tag-name should route to #index?tag=the-tag-name&page=1" do
+      {:get => '/tag/the-tag-name/page/7'}.should route_to(
+        :controller => 'posts',
+        :action => 'index',
+        :tag => 'the-tag-name',
+        :page => '7'
+      )
+    end
+  end
+  
+end
