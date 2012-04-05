1fa06a5eecfaffd872d5a0d63de849f01d55167f
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-04-05 13:06:16 -0300

    Added tags to the post lits

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
 require "#{Rails.root}/spec/factories.rb"
 
-0.upto(137) do |n|
+PostTag.delete_all
+Post.where('title like "title%"').delete_all
+Tag.delete_all
+
+
+@tag1 = Factory(:tag)
+@tag2 = Factory(:tag)
+@tag3 = Factory(:tag)
+
+0.upto(30) do |n|
   Factory(:post, :publication_date => n.days.ago)
+  Factory(:post, :publication_date => n.days.ago, :tags => [@tag1])
+  Factory(:post, :publication_date => n.days.ago, :tags => [@tag2])
+  Factory(:post, :publication_date => n.days.ago, :tags => [@tag1,@tag2,@tag3])
 end
+
+
+
+
